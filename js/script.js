ca357b6e798cdde13b4f9fd4bc6f4f06b9f9097a
KrazIvan <109521157+KrazIvan@users.noreply.github.com>
2023-03-05 19:25:58 +0100

    Adding my portfolio site.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("background-music").volume = 0.5;
+
+if (document.getElementById('my-work-link')) {
+  document.getElementById('my-work-link').addEventListener('click', () => {
+    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+  })
+}
