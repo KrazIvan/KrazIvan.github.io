84b9188cb9bac35b41cbab6a73cf91fa88f91a7b
KrazIvan <109521157+KrazIvan@users.noreply.github.com>
2023-03-23 21:18:22 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+const lightSwitchSounds = [
+  "./assets/audio/light_switch_audio1.wav",
+  "./assets/audio/light_switch_audio2.wav",
+  "./assets/audio/light_switch_audio3.wav",
+  "./assets/audio/light_switch_audio4.wav",
+  "./assets/audio/light_switch_audio5.wav"
+];
+
+function playRandomSound() {
+  const randomIndex = Math.floor(Math.random() * sounds.length);
+  const audio = new Audio(sounds[randomIndex]);
+  audio.play();
+}
+
+function darkMode() {
+  playRandomSound();
+}
