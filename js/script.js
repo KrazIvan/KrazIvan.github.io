e10be621ed09ba69526efe87ab576ce0d0b65479
KrazIvan <109521157+KrazIvan@users.noreply.github.com>
2023-03-23 22:25:37 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 //document.getElementById("background-music").volume = 0.5;
+var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
 
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
@@ -21,8 +22,7 @@ function playRandomSound(soundArray) {
 }
 
 function darkMode() {
-  playRandomSound(lightSwitchSounds);
-  var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024); 
+  playRandomSound(lightSwitchSounds); 
 
   db.transaction(function (tx) { 
     tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (id unique, log)'); 
