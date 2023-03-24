372d13edbbaddd073648c53621a8eb7d68bbeeb5
KrazIvan <109521157+KrazIvan@users.noreply.github.com>
2023-03-24 15:18:02 +0100

    Dark mode functionality added.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 //document.getElementById("background-music").volume = 0.5;
 var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
+var now = new Date();
+var nowHour = now.getHours();
 
-if (document.getElementById('my-work-link')) {
-  document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+if (document.getElementById("my-work-link")) {
+  document.getElementById("my-work-link").addEventListener("click", () => {
+    document.getElementById("my-work-section").scrollIntoView({behavior: "smooth"})
   })
+}
+
+if (nowHour >= 18 || nowHour < 8) {
+  const body = document.body;
+  const navTitle = document.querySelector(".nav-title");
+  const buttons = document.querySelectorAll(".button");
+  
+  body.classList.toggle("dark-mode");
+  navTitle.classList.toggle("dark-mode");
+  
+  buttons.forEach(button => {
+    button.classList.toggle("dark-mode");
+  });
 }
 
 const lightSwitchSounds = [
@@ -22,11 +37,23 @@ function playRandomSound(soundArray) {
 }
 
 function darkMode() {
+  const body = document.body;
+  const navTitle = document.querySelector(".nav-title");
+  const buttons = document.querySelectorAll(".button");
+  
+  body.classList.toggle("dark-mode");
+  navTitle.classList.toggle("dark-mode");
+  
+  buttons.forEach(button => {
+    button.classList.toggle("dark-mode");
+  });
+  
   playRandomSound(lightSwitchSounds); 
-
-  db.transaction(function (tx) { 
-    tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (id unique, log)'); 
-    tx.executeSql('INSERT INTO LOGS (id, log) VALUES (1, "foobar")'); 
-    tx.executeSql('INSERT INTO LOGS (id, log) VALUES (2, "logmsg")'); 
- }); 
+ 
 }
+
+//db.transaction(function (tx) { 
+//  tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (id unique, log)'); 
+//  tx.executeSql('INSERT INTO LOGS (id, log) VALUES (1, "foobar")'); 
+//  tx.executeSql('INSERT INTO LOGS (id, log) VALUES (2, "logmsg")'); 
+//});
